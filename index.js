07a039b0f495f6318d38f5d4e6f70d26fe414fad
katehampton <katehampton29@gmail.com>
2021-06-30 12:00:44 +1200

    adding questions

--- index.js
+++ index.js
@@ -1,1 +1,4 @@
-
+var questions = [
+    new Question("How many Essential Vitamins are there?", ["18","21","13","16"], "13")
+    new Question("What is the Chemical Name for Vitamin C?", ["cyanocobalamin", "folic acid","biotin"," ascorbic acid"],"ascorbic acid")
+];
