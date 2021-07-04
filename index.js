7844e25f175a05f0d7c3d9a719bad8cb4a91a501
katehampton <katehampton29@gmail.com>
2021-07-05 09:16:39 +1200

    google translate

--- index.js
+++ index.js
@@ -1,8 +1,3 @@
-var questions = [
-    new Question("How many Essential Vitamins are there?", ["18","21","13","16"], "13")
-    new Question("What is the Chemical Name for Vitamin C?", ["cyanocobalamin", "folic acid","biotin"," ascorbic acid"],"ascorbic acid")
-];
-
 function googleTranslateElementInit() {
     new google.translate.TranslateElement({pageLanguage: 'en'}, 'google_translate_element');
   };
