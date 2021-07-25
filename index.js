7a310ac04647686463e97bd9faa519aa03359512
katehampton <katehampton29@gmail.com>
2021-07-26 10:31:51 +1200

    quiz

--- index.js
+++ index.js
@@ -1,10 +1,7 @@
 const Question = document.querySelector("#question");
 const Answer = document.querySelector("#answer");
 const Verdict = document.querySelector("#verdict");
-const questionList = ["2 + 2", "3 + 3", "4 + 4"];
-const answerList = ["4", "6", "8"];
-const congratsList = ["Correct", "Well Done","Amazing"];
-const worngList = ["Incorrect", "NOPE.", "NO! You're Wrong"];
+
 
 var number = 0;
 
@@ -21,9 +18,11 @@ function quizFunction() {
     }
     text = "";
 }
+var questions = [
+    new Question("How many Essential Vitamins are there?",
+ [ "12", "15", "13", "11" ], "11"),
 
 function nextQuestion() {
     number += 1;
     Question.innerHTML = text;
-
 }
