611f676e4ace9c309dbc599ed991cd1ec1bf413b
katehampton <katehampton29@gmail.com>
2021-09-20 17:22:41 +1200

    html for quiz side-bar nav

--- index.js
+++ index.js
@@ -1,25 +1,7 @@
-const Question = document.querySelector("#question");
-const Answer = document.querySelector("#answer");
-const Verdict = document.querySelector("#verdict");
-const questionList = ["How many essential vitamins are there", "What is the chemical name for Vitamin C", "How many chemical names are there for vitamin E", ];
-const answerList = ["13", "Ascorbic Acid.", "2"];
-const congratsList = ["Correct!", "Well Done!", "Amazing!"];
-const wrongList = ["Incorrect", "Nope.", "No! You're wrong!"];
-var number = 0; 
-let text = questionList[number] + "?";
-Question.innerHTML = text;
-function quizFunction() {
-    if (Answer.value == answerList[number]) {
-        document.body.style.backgroundColor = "Green";
-        Verdict.innerHTML = congratsList[number];
-    } else {
-        document.body.style.backgroundColor = "Red";
-        Verdict.innerHTML = wrongList[number];
-    }
-    text = "";
-}
-function nextQuestion() {
-    number += 1;
-    text += questionList[number] + "?";
-    Question.innerHTML = text;
-}
+function openNav() {
+    document.getElementById("mySidenav").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidenav").style.width = "0";
+  }
